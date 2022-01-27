23a7c61a807bb37c403163a2eb2a8242605779c9
mar-stiv <mariastivala.ms@gmail.com>
2022-01-27 23:35:58 +0100

    SERVER IS LE DONE
    ok now some more status codes and we gucci

--- ass3/server.js
+++ ass3/server.js
@@ -1,20 +1,6 @@
 // ###############################################################################
 // Web Technology at VU University Amsterdam
 // Assignment 3
-//
-// The assignment description is available on Canvas.
-// Please read it carefully before you proceed.
-//
-// This is a template for you to quickly get started with Assignment 3.
-// Read through the code and try to understand it.
-//
-// Have you read the zyBook chapter on Node.js?
-// Have you looked at the documentation of sqlite?
-// https://www.sqlitetutorial.net/sqlite-nodejs/
-//
-// Once you are familiar with Node.js and the assignment, start implementing
-// an API according to your design by adding routes.
-
 // ###############################################################################
 //
 // Database setup:
@@ -22,110 +8,7 @@
 // We are going to use the variable "db' to communicate to the database:
 // If you want to start with a clean sheet, delete the file 'phones.db'.
 // It will be automatically re-created and filled with one example item.
-
-const sqlite = require('sqlite3').verbose();
-let db = my_database('./phones.db');
-
-// ###############################################################################
-// The database should be OK by now. Let's setup the Web server so we can start
-// defining routes.
-//
-// First, create an express application `app`:
-
-const express = require("express");
-const router = express.Router(); // import routes
-
-const app = express();
-app.use(express.json());
-
-// We need some middleware to parse JSON data in the body of our HTTP requests:
-var bodyParser = require("body-parser");
-app.use(bodyParser.json()); // adds a new middleware to the app which configures body parser to parse JSON
-app.use(bodyParser.urlencoded({
-   extended: true // restricts body to json
-}));
-
-// ###############################################################################
-// Routes
-//
-// TODO: Add your routes here and remove the example routes once you know how
-//       everything works.
-router.get('/home',function(req,res){
-  response_body = {'message': 'ping successful'};
-  res.json(response_body);
-});
-
-// POST method route
-app.get('/newphone', function(req,res){ // dummy function
-  res.send('POST new phone');
-});
-
-// retrieve full data set (all rows in product database)
-
-// add data for a new product item (create)
-
-// list data of a specific item (retrieve)
-
-// change data of a specific item (update)
-
-// remove data of a specific item (delete)
-// ###############################################################################
-
-app.use('/',router); // if any request comes with the ‘/’, it will call the router function with its route.
-
-// This example route responds to http://localhost:3000/hello with an example JSON object.
-// Please test if this works on your own device before you make any changes.
-
-//reset database to first two items only
-app.get("/hello", function(req, res) { // Routes HTTP GET requests to the specified path with the specified callback functions.
-    response_body = {'Hello': 'World'} ;
-
-    // This example returns valid JSON in the response, but does not yet set the
-    // associated HTTP response header.  This you should do yourself in your
-    // own routes!
-    res.json(response_body);
-    res.send('GET request to homepage');
-});
-
-// This route responds to http://localhost:3000/db-example by selecting some data from the
-// database and return it as JSON object.
-// Please test if this works on your own device before you make any changes.
-
-//
-app.get('/database', function(req, res) {
-    // Example SQL statement to select the name of all products from a specific brand
-    db.all(`SELECT * FROM phones WHERE brand=?`, ['Fairphone'], function(err, rows) {
-
-    	// TODO: add code that checks for errors so you know what went wrong if anything went wrong
-      if (err) {
-        console.log(err);
-        res.sendStatus(500);
-        return;
-      }
-
-    	// TODO: set the appropriate HTTP response headers and HTTP response codes here.
-
-
-    	// # Return db response as JSON
-    	return res.json(rows)
-    });
-});
-
-//
-app.post('/post-example', function(req, res) {
-	// This is just to check if there is any data posted in the body of the HTTP request:
-	console.log(req.body);
-	return res.json(req.body);
-});
-
-// ###############################################################################
-// This should start the server, after the routes have been defined, at port 3000:
-app.listen(3000, () =>{
-  console.log("Your Web server should be up and running, waiting for requests to come in. Try http://localhost:3000/hello");
-}); // Binds and listens for connections on the specified host and port. This method is identical to Node’s http.Server.listen().
-
-// ###############################################################################
-// Some helper functions called above
+// Some helper function:
 function my_database(filename) {
 	// Conncect to db by opening filename, create filename if it does not exist:
 	var db = new sqlite.Database(filename, (err) => {
@@ -157,3 +40,99 @@ function my_database(filename) {
 	});
 	return db;
 }
+
+const sqlite = require('sqlite3').verbose();
+let db = my_database('./phones.db');
+
+const express = require("express");
+const router = express.Router(); // import routes
+
+const app = express();
+app.use(express.json());
+
+// We need some middleware to parse JSON data in the body of our HTTP requests:
+var bodyParser = require("body-parser");
+app.use(bodyParser.json()); // adds a new middleware to the app which configures body parser to parse JSON
+app.use(bodyParser.urlencoded({
+   extended: true // restricts body to json
+}));
+
+// ###############################################################################
+// Routes for http://localhost:3000/api
+
+// retrieve full data set (all rows in product database) - GET - WORKS
+app.get("/api/phones", (req,res,next) =>{
+  db.all("SELECT id, brand, model, os, image, screensize FROM phones", [], (err,rows) =>{
+    if(err){
+      res.status(400).json({"error":err.message});
+      return;
+    }
+    res.status(200).json({rows});
+  })
+});
+
+// add data for a new product item (create) - POST - FIXING: "error": "SQLITE_ERROR: near \"brand\": syntax error"
+app.post("/api/insert", (req, res, next) => {
+  reqBody = req.body;
+  db.run("INSERT INTO phones (brand, model, os, image, screensize) VALUES (?, ?, ?, ?, ?)",
+    [reqBody.brand, reqBody.model, reqBody.os, reqBody.image, reqBody.screensize],
+    function (err, result) {
+      if (err) {
+        res.status(400).json({ "error": err.message })
+        return;
+      }
+      res.status(201).json({ "phone_id": this.lastID });
+    })
+});
+
+// list data of a specific item (retrieve) - GET - WORKS
+app.get("/api/phones/:id", (req,res,next) => {
+  var params = req.params.id;
+    db.all("SELECT id, brand, model, os, image, screensize FROM phones WHERE id= ?",
+    params,
+    (err, row) =>{
+      if(err){
+        res.status(400).json({"error":err.message});
+        return;
+      }
+      if(row.length>0){
+        res.status(200).json(row);
+      }else{
+
+      }
+    });
+});
+
+// change data of a specific item (update) - PUT - WORKS
+app.put("/api/update/:id", (req,res,next) =>{ // filers phone ID selected
+  item = req.body;
+  db.run(`UPDATE phones SET brand=?, model=?, os=?, image=?,screensize=? WHERE id=?`,
+     [item.brand, item.model, item.os, item.image, item.screensize, req.params.id],
+     function(err,result){
+       if(err){
+         res.status(400).json({"error": res.message})
+         return;
+       }
+       res.status(201).json({"updatedID": req.params.id});
+     });
+});
+
+// remove data of a specific item (delete) - DELETE
+app.delete("/api/remove/:id", (req, res, next) =>{
+  db.run("DELETE FROM phones WHERE id = ?",
+  req.params.id,
+    function(err,result){
+      if(err){
+        res.status(400).json({"error":res.message})
+        return;
+      }
+      res.status(200).json({"deletedID":req.params.id})
+    });
+});
+// ###############################################################################
+// This should start the server, after the routes have been defined, at port 3000:
+app.listen(3000, () =>{
+  console.log("Your Web server should be up and running, waiting for requests to come in. Try http://localhost:3000/hello");
+}); // Binds and listens for connections on the specified host and port. This method is identical to Node’s http.Server.listen().
+
+// ###############################################################################
